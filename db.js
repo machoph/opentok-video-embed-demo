3bd8b2eb39f04138156ecfa0638eaa8cdd3c7a0c
Oscar Phillips <machoph@gmail.com>
2019-08-21 06:38:24 -0600

    add nconf implementation

--- db.js
+++ db.js
@@ -1,8 +1,9 @@
 // This is our simple DB in memory. A real-world use case would use an actual database.
+var config  = require('./conf');
 let DB = {
 
   embed_code: `<div id="otEmbedContainer" style="width:100%; height:100%"></div>
-                    <script src="https://tokbox.com/embed/embed/ot-embed.js?embedId=c2a324bf-a9aa-44fa-9e89-7e4a66d1a888&room=DEFAULT_ROOM"></script>`
+                    <script src="https://tokbox.com/embed/embed/ot-embed.js?embedId=${config.get("OPENTOK_EMBEDED_ID")}&room=DEFAULT_ROOM"></script>`
 };
 
 
